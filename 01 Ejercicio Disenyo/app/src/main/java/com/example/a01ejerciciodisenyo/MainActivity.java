5314d74db9c3bdf598320bfae0a9dfa364bd02a7
MonsBaan <ibai3553@gmail.com>
2021-09-10 10:40:57 +0200

    Toast Fancy

--- 01 Ejercicio Disenyo/app/src/main/java/com/example/a01ejerciciodisenyo/MainActivity.java
+++ 01 Ejercicio Disenyo/app/src/main/java/com/example/a01ejerciciodisenyo/MainActivity.java
@@ -3,8 +3,13 @@ package com.example.a01ejerciciodisenyo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
@@ -15,14 +20,39 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ImageButton button = (ImageButton)findViewById(R.id.imgBtnCalendario);
-        button.setOnClickListener(this::onClick);
+        ImageButton btnNuevo = findViewById(R.id.imgBtnNuevo);
+
+        //CREAR TOAST PERSONALIZADO
+        final int duracion = Toast.LENGTH_SHORT;
+        LayoutInflater inflater = getLayoutInflater();
+        final View miToast = inflater.inflate(R.layout.toast_per, (ViewGroup) findViewById(R.id.layoutToast));
+        final Toast toast = new Toast(getApplicationContext());
+
+        //CAMBIAR TEXTO A TOAST PERSONALIZADO
+        TextView textoToast = miToast.findViewById(R.id.tvToast);
+        textoToast.setText("Albertxigo");
+
+        //CAMBIAR IMAGEN A TOAST PERSONALIZADO
+        ImageView fotoToast =miToast.findViewById(R.id.ivToast);
+        fotoToast.setImageResource(R.drawable.ic_launcher_background);
+
+
+        btnNuevo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                /*Toast Cutre
+                Toast.makeText(getApplicationContext(), "Cagaste Guacho", Toast.LENGTH_LONG).show();*/
+
+                //Toast Fancy
+                toast.setView(miToast);
+                toast.setGravity(Gravity.CENTER_VERTICAL, 0, 300);
+                toast.setDuration(duracion);
+                toast.show();
+            }
+        });
     }
 
 
-    public void onClick(View v){
-        Toast.makeText(this, "Cagaste Guacho",Toast.LENGTH_LONG).show();
-    }
 
 
 
