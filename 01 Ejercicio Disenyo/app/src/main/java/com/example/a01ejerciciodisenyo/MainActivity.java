7511ae72e5332666917e867f0db26a1d2b4ad0c2
MonsBaan <ibai3553@gmail.com>
2021-09-10 09:03:52 +0200

    Ejercicio 1 Button Event Listener

--- 01 Ejercicio Disenyo/app/src/main/java/com/example/a01ejerciciodisenyo/MainActivity.java
+++ 01 Ejercicio Disenyo/app/src/main/java/com/example/a01ejerciciodisenyo/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.a01ejerciciodisenyo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageButton button = (ImageButton)findViewById(R.id.imgBtnCalendario);
+        button.setOnClickListener(this::onClick);
     }
+
+
+    public void onClick(View v){
+        Toast.makeText(this, "Cagaste Guacho",Toast.LENGTH_LONG).show();
+    }
+
+
+
+
+    /* Toast OnClick activity_main.xml
+
+    public void toastMsg(View view) {
+        Toast toast = Toast.makeText(this, "Cagaste Guacho",Toast.LENGTH_LONG);
+        toast.show();
+    }*/
 }
