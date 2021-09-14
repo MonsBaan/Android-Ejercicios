a843b7ec5aaef0c212c166877fe547783a44fd07
MonsBaan <ibai3553@gmail.com>
2021-09-14 10:20:53 +0200

    Adaptador ListView

---
+++ 03 Adaptadores/app/src/main/java/com/example/a03adaptadores/CompaniasAdapter.java
@@ -0,0 +1,56 @@
+package com.example.a03adaptadores;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class CompaniasAdapter extends BaseAdapter {
+
+    private Context context = null;
+    ArrayList<CompaniaTelefonica> companiaTelefonicas = new ArrayList<CompaniaTelefonica>();
+
+    public CompaniasAdapter(Context context, ArrayList<CompaniaTelefonica> companiaTelefonicas) {
+        this.context = context;
+        this.companiaTelefonicas = companiaTelefonicas;
+    }
+
+    @Override
+    public int getCount() {
+        return companiaTelefonicas.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return companiaTelefonicas.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        LayoutInflater inflater = ((Activity) context).getLayoutInflater();
+        View fila = inflater.inflate(R.layout.compania_telefonica_list, null);
+
+        CompaniaTelefonica compania = companiaTelefonicas.get(i);
+        ImageView ivCompañia = fila.findViewById(R.id.ivCompaniaTelefonica);
+        ivCompañia.setImageResource(compania.getLogo());
+
+        TextView tvNombre = fila.findViewById(R.id.tvNombreCompania);
+        tvNombre.setText(compania.getNombre());
+
+        TextView tvPrecio = fila.findViewById(R.id.tvPrecioCompania);
+        tvPrecio.setText(compania.getPrecio() + "");
+        return fila;
+    }
+}
--- 03 Adaptadores/app/src/main/java/com/example/a03adaptadores/ListActivity.java
+++ 03 Adaptadores/app/src/main/java/com/example/a03adaptadores/ListActivity.java
@@ -3,12 +3,29 @@ package com.example.a03adaptadores;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class ListActivity extends AppCompatActivity {
+    private ArrayList<CompaniaTelefonica> companias = new ArrayList<>();
+    private ListView listaCompanias;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+
+        rellenarCompanias();
+        CompaniasAdapter adapter = new CompaniasAdapter(this, companias);
+        listaCompanias = findViewById(R.id.lvTelefonicas);
+        listaCompanias.setAdapter(adapter);
+    }
+
+    private void rellenarCompanias(){
+        companias.add(new CompaniaTelefonica("Movistar", R.drawable.a1, 86));
+        companias.add(new CompaniaTelefonica("Euskaltel", R.drawable.a2, 14));
+        companias.add(new CompaniaTelefonica("Vodafone", R.drawable.andando1, 53));
+        companias.add(new CompaniaTelefonica("Yoigo", R.drawable.ic_launcher_background, 65));
     }
 }
