c26b4ab7f6ebaf78539972009cd4a5fd5d21f56b
MonsBaan <ibai3553@gmail.com>
2021-09-22 18:29:23 +0200

    Supuestamente mi dolor termina aqui

--- Proyecto Individual/app/src/main/java/com/example/proyectoindividual/ui/inicio/FragmentInicioViewModel.java
+++
@@ -1,7 +0,0 @@
-package com.example.proyectoindividual.ui.inicio;
-
-import androidx.lifecycle.ViewModel;
-
-public class FragmentInicioViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
